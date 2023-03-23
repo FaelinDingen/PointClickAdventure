7c404f21cc3ac10c77cc7da04003a83c2b04d72f
FaelinDingen <127936791+FaelinDingen@users.noreply.github.com>
2023-03-23 12:34:56 +0100

    did stuff

--- js/index.js
+++ js/index.js
@@ -1,30 +1,76 @@
 const mainCharacter = document.getElementById("mainCharacter");
 const gameWindow = document.getElementById("gameWindow");
-const offsetCharacter = 16;
 const characterAudio = document.getElementById("characterAudio");
+const mcBubble = document.getElementById("mcBubble");
+let hasPick = false;
+let rocked = false;
 
 document.getElementById('ground').setAttribute('draggable', false);
+document.getElementById('foregroundImg').setAttribute('draggable', false);
 
 gameWindow.onclick = async function (e) {
 
     var rect = gameWindow.getBoundingClientRect();
-    var x = e.clientX - rect.left - offsetCharacter;
-    var y = e.clientY - rect.top - offsetCharacter;
+    var x = e.clientX - rect.left - 16;
+    var y = e.clientY - rect.top - 16;
     mainCharacter.style.left = x + "px";
     mainCharacter.style.top = y + "px";
 
     switch (e.target.id) {
         case ("tool"):
             await delay(2500);
-            characterAudio.play();
-            document.getElementById("ground").src = "img/TileMap2.png";
+            showSpeech();
+            if (!hasPick) {
+                characterAudio.play();
+                document.getElementById("ground").src = "img/TileMap2.png";
+                hasPick = true;
+                showSpeech("Nice! A pickaxe");
+            }
+            else {
+                showSpeech("Nothing else in here");
+            }
             break;
-        case ("stone"):
-
+        case ("rock"):
+            await delay(2500);
+            showSpeech();
+            if (hasPick) {
+                characterAudio.play();
+                document.getElementById("ground").src = "img/TileMap3.png";
+                showSpeech("There's a cool switch\nI hear a contraption activating");
+                rocked = true;
+                break;
+            }
+            else if (!hasPick) {
+                showSpeech("U see something peaking out underneath the stone\nIf only u have a tool to break it open with");
+            }
+            else {
+                showSpeech("A pile of small rocks with a flicked switch underneath");
+            }
+        case ("cave"):
+            await delay(2500);
+            if (rocked) {
+                document.getElementById("foregroundImg").src = "img/TileMapTrans.png";
+                await delay(2000);
+                document.getElementById("foregroundImg").src = "img/TileMapForeGround2.png";
+                document.getElementById("ground").src = "img/TileMap4.png";
+            }
+            break;
+        default:
+            hideSpeech();
             break;
     }
 }
 
+
+function showSpeech(dialogue) {
+    mcBubble.style.opacity = 1;
+    mcBubble.innerText = dialogue;
+}
+
+function hideSpeech() {
+    mcBubble.style.opacity = 0;
+    mcBubble.innerText = "...";
+}
 
 function delay(milliseconds) {
     return new Promise(resolve => {
