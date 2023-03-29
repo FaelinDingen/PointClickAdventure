90d9ea3dcae72716d95c6658285a454afac33f5e
FaelinDingen <127936791+FaelinDingen@users.noreply.github.com>
2023-03-29 16:51:14 +0200

    added a proper ending

--- js/index.js
+++ js/index.js
@@ -37,8 +37,13 @@ gameWindow.onclick = async function (e) {
                     document.getElementById("startText").innerText = "You are the last hope of this world";
                     await delay(4000);
                     document.getElementById("startText").innerText = "You, the last time wizzard\nneed to find the currupted time crystal\nAND BREAK IT";
-                    await delay(6000);
-                    document.getElementById("playButton").remove();
+                    await delay(2000);
+                    document.getElementById("playButton").style.backgroundColor = 'rgba(0, 0, 0, 0)';
+                    document.getElementById("startText").style.color = 'rgba(0, 0, 0, 0)';
+                    await delay(3000);
+                    document.getElementById("playButton").classList.toggle("foreground3");
+                    document.getElementById("playButton").classList.toggle("disabled");
+                    document.getElementById("startText").classList.toggle("disabled");
                     canMove = true;
                 }
                 break;
@@ -50,6 +55,7 @@ gameWindow.onclick = async function (e) {
                     document.getElementById("ground").src = "Img/TileMap2.png";
                     hasPick = true;
                     showSpeech("Nice! A pickaxe");
+                    document.getElementById("toolItem").classList.toggle("disabled");
                 }
                 else {
                     showSpeech("Nothing else in here");
@@ -61,7 +67,8 @@ gameWindow.onclick = async function (e) {
                 showSpeech();
                 if (hasPick && !rocked) {
                     document.getElementById("ground").src = "Img/TileMap3.png";
-                    showSpeech("There's a switch\nI hear a contraption activating");
+                    showSpeech("There's a switch\nI hear a contraption activating\nHowever the pick broke");
+                    document.getElementById("toolItem").classList.toggle("disabled");
                     rocked = true;
                 }
                 else if (!rocked) {
@@ -80,6 +87,7 @@ gameWindow.onclick = async function (e) {
                     mainCharacter.style.left = 375 + "px";
                     mainCharacter.style.top = 510 + "px";
                     document.getElementById("ground").src = "Img/DungeonTile" + dungeonState + ".png";
+                    document.body.style.backgroundImage = 'url(Img/Background2.png)';
                     document.getElementById("foregroundImg").src = "Img/Empty.png";
                     characterAudio.src = "Audio/TalkingInside.mp3";
                     canMove = false;
@@ -104,8 +112,26 @@ gameWindow.onclick = async function (e) {
                 showSpeech("An old book, it reads: \nThe key to the clean steel lies at the shrine on the north wall");
                 break;
             case ("bed"):
-                await delay(2500);
-                showSpeech("No time to rest!\nThe world needs to be saved!");
+                await delay(3000);
+                if (ended) {
+                    document.getElementById("playButton").classList.toggle("foreground3");
+                    document.getElementById("playButton").classList.toggle("disabled");
+                    document.getElementById("startText").classList.toggle("disabled");
+                    document.getElementById("playButton").style.backgroundColor = 'rgba(0, 0, 0, 1)';
+                    document.getElementById("startText").style.color = 'rgba(255, 255, 255, 1)';
+                    document.getElementById("startText").innerText = 'You, The Hero have completed your mission\nU head to bed and close your eyes';
+                    canMove = false;
+                    await delay(3000);
+                    document.getElementById("startText").style.color = 'rgba(0, 0, 0, 0)';
+                    await delay(3000);
+                    document.getElementById("startText").style.color = 'rgba(255, 255, 255, 1)';
+                    document.getElementById("startText").innerText = 'You wake up and see a mysterious black and white figure\nstanding the the base of your bed';
+                    await delay(4000);
+                    document.getElementById("startText").innerText = 'End of the game\nTo be continued in : Point And click adventure 2, The clickening!';
+                }
+                else {
+                    showSpeech("No time to rest!\nThe world needs to be saved!");
+                }
                 break;
             case ("boxes"):
                 await delay(2500);
@@ -116,6 +142,7 @@ gameWindow.onclick = async function (e) {
                 if (!hasKey) {
                     showSpeech("A key is poking out\nU take it");
                     hasKey = true;
+                    document.getElementById("keyItem").classList.toggle("disabled");
                 }
                 else {
                     showSpeech("An empty shrine");
@@ -124,7 +151,9 @@ gameWindow.onclick = async function (e) {
             case ("chest"):
                 await delay(2500);
                 if (hasKey && !hasAcid) {
-                    showSpeech("U unlock the chest and open it\nThere is a bottle of acid inside");
+                    showSpeech("U unlock the chest and open it\nThere is a bottle of acid inside\nThe key broke");
+                    document.getElementById("keyItem").classList.toggle("disabled");
+                    document.getElementById("acidItem").classList.toggle("disabled");
                     hasAcid = true;
                     document.getElementById("ground").src = "Img/DungeonTile2.png";
                     dungeonState++;
@@ -140,6 +169,7 @@ gameWindow.onclick = async function (e) {
                 await delay(2500);
                 if (hasAcid && !unrusted) {
                     showSpeech("U let the acid desolve the rust on the lever\nu switch the lever");
+                    document.getElementById("acidItem").classList.toggle("disabled");
                     unrusted = true;
                     document.getElementById("ground").src = "Img/DungeonTile3.png";
                     dungeonState++;
@@ -182,13 +212,16 @@ gameWindow.onclick = async function (e) {
                 if (brokeCrystal) {
                     document.getElementById("foregroundImg").src = "Img/TileMapForeGround2.png";
                     document.getElementById("ground").src = "Img/TileMap4.png";
+                    document.body.style.backgroundImage = 'url(Img/Background3.png)';
                     if (!ended) {
-                        showSpeech("Its Fall!\nThe world is saved!");
+                        showSpeech("Its Fall!\nThe world is saved!\nYou are exhausted and can finally get some rest");
+                        ended = true;
                     }
                 }
                 else {
                     document.getElementById("foregroundImg").src = "Img/TileMapForeGround1.png";
                     document.getElementById("ground").src = "Img/TileMap3.png";
+                    document.body.style.backgroundImage = 'url(Img/Background1.png)';
                 }
                 inCave = false;
                 break;
